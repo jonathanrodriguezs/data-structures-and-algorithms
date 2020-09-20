1168f436b4d9e1210e9c60496ffc19f6831cd85f
Jonathan Rodriguez Sanchez <jonathan.rodriguezs@softtek.com>
2020-09-20 15:29:33 -0500

    feat(arrays): add swap function to array_util.h

--- interview-questions/arrays/array_rotate.c
+++ interview-questions/arrays/array_rotate.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+#include "array_util.h"
+#define N 4
+
+void rotate(int nums[], int nums_size, int k)
+{
+}
+
+int main(void)
+{
+  int nums[N] = {-1, -100, 3, 99};
+  rotate(nums, N, 2);
+  // Expected nums to be [3,99,-1,-100]
+  print_array(nums, N);
+  return 0;
+}
--- interview-questions/arrays/array_util.h
+++ interview-questions/arrays/array_util.h
@@ -5,3 +5,11 @@ void print_array(int *array, int size)
   for (int i = 0; i < size; i++)
     printf("A[%d] = %d \n", i, *array++);
 }
+
+void swap(int *ptr1, int *ptr2)
+{
+  int temp;
+  temp = *ptr1;
+  *ptr1 = *ptr2;
+  *ptr2 = temp;
+}
