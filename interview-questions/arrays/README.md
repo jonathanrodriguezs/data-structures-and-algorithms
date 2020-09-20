279f4c5c9fea1294630f5f51ac28301fe0060f29
Jonathan Rodriguez Sanchez <jonathan.rodriguezs@softtek.com>
2020-09-20 16:41:02 -0500

    feat(arrays): add rotate program

--- interview-questions/arrays/README.md
+++ interview-questions/arrays/README.md
@@ -1,4 +1,11 @@
 # Arrays interview questions
 
+## Easy
+
 - [Remove duplicates](./array_remove-duplicates.c):
   Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+
+## Medium
+
+- [Rotate Array](./array_rotate.c):
+  Given an array, rotate the array to the right by k steps, where k is non-negative.
--- interview-questions/arrays/array_rotate.c
+++ interview-questions/arrays/array_rotate.c
@@ -4,13 +4,28 @@
 
 void rotate(int nums[], int nums_size, int k)
 {
+  k = k % nums_size;
+  int count = 0;
+  for (int i = 0; count < nums_size; i++)
+  {
+    int current = i;
+    int prev = nums[current];
+    do
+    {
+      int next = (current + k) % nums_size;
+      int temp = nums[next];
+      nums[next] = prev;
+      prev = temp;
+      current = next;
+      count++;
+    } while (current != i);
+  }
 }
 
 int main(void)
 {
   int nums[N] = {-1, -100, 3, 99};
-  rotate(nums, N, 2);
-  // Expected nums to be [3,99,-1,-100]
+  rotate(nums, N, 2); // Expected: [3,99,-1,-100]
   print_array(nums, N);
   return 0;
 }
