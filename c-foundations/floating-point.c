0387103c1f31c372f980374371532a9bdeff2b52
Jonathan Rodriguez <55515743+jonathanrodriguezs@users.noreply.github.com>
2020-09-30 21:48:09 -0500

    Update floating-point.c

--- c-foundations/floating-point.c
+++ c-foundations/floating-point.c
@@ -10,35 +10,37 @@
 // Little endian: IEEE 754 32 bits float
 union float_unmask_le {
   float single_float;
-  struct {
-    unsigned long mantissa: 23;
-    unsigned int exponent: 8;
-    unsigned int sign: 1;
-  }
-  pack;
+  struct
+  {
+    unsigned long mantissa : 23;
+    unsigned int exponent : 8;
+    unsigned int sign : 1;
+  } pack;
 };
 
 // Type definitions
 typedef union float_unmask_le float_unmask_le;
 // Function prototypes
 float reverse_bytes_float(const float n);
-void print_float_bytes(char * ptr);
+void print_float_bytes(char *ptr);
 bool is_little_endian(void);
-void print_float_unmask(float_unmask_le s_float, char * formatter);
-void print_header(char * header);
-void print_results(float x, char * ptr);
+void print_float_unmask(float_unmask_le s_float, char *formatter);
+void print_header(char *header);
+void print_results(float x, char *ptr);
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[])
+{
   float x = 0.0;
   bool little_endian = is_little_endian();
   printf("Floating point number: ");
   scanf("%f", &x);
-  
-  char * ptr = (char * ) & x;
 
-  if (little_endian) {
+  char *ptr = (char *)&x;
+
+  if (little_endian)
     print_results(x, ptr);
-  } else {
+  else
+  {
     printf("Not compatible architecture");
     return 1;
   }
@@ -47,10 +49,11 @@ int main(int argc, char * argv[]) {
 }
 
 // Reverse the bytes of a single float in another buffer
-float reverse_bytes_float(float n) {
+float reverse_bytes_float(float n)
+{
   float reversed;
-  char * float_ptr = (char * ) & n;
-  char * reversed_ptr = (char * ) & reversed;
+  char *float_ptr = (char *)&n;
+  char *reversed_ptr = (char *)&reversed;
 
   // Swap the bytes into a temporary buffer
   reversed_ptr[0] = float_ptr[3];
@@ -62,37 +65,45 @@ float reverse_bytes_float(float n) {
 }
 
 // Print the four bytes in a single float in hex format
-void print_float_bytes(char * ptr) {
-  printf("%X ", (unsigned char) * ptr);
-  printf("%X ", (unsigned char) * (ptr + 1));
-  printf("%X ", (unsigned char) * (ptr + 2));
-  printf("%X ", (unsigned char) * (ptr + 3));
+void print_float_bytes(char *ptr)
+{
+  printf("%X ", (unsigned char)*ptr);
+  printf("%X ", (unsigned char)*(ptr + 1));
+  printf("%X ", (unsigned char)*(ptr + 2));
+  printf("%X ", (unsigned char)*(ptr + 3));
 }
 
 // Print sign, exponent and mantissa of a 32 bits float
-void print_float_unmask(float_unmask_le s_float, char * formatter) {
-  printf("Sign: ");     printf(formatter, s_float.pack.sign);
-  printf("Exponent: "); printf(formatter, s_float.pack.exponent);
-  printf("Mantissa: ");  printf(formatter, s_float.pack.mantissa);
+void print_float_unmask(float_unmask_le s_float, char *formatter)
+{
+  printf("Sign: ");
+  printf(formatter, s_float.pack.sign);
+  printf("Exponent: ");
+  printf(formatter, s_float.pack.exponent);
+  printf("Mantissa: ");
+  printf(formatter, s_float.pack.mantissa);
 }
 
 // Determine if the host uses little endian
-bool is_little_endian(void) {
+bool is_little_endian(void)
+{
   int16_t i = 1;
-  int8_t * p = (int8_t * ) & i;
+  int8_t *p = (int8_t *)&i;
   return (p[0] == 1);
 }
 
 // Cleaner prints
-void print_header(char * header) {
+void print_header(char *header)
+{
   printf("\n\n* %s:\n", header);
 }
 
 // Print the results to stdout
-void print_results(float x, char * ptr) {
+void print_results(float x, char *ptr)
+{
   float reversed = reverse_bytes_float(x);
-  char * ptr_r = (char * ) & reversed;
-  float_unmask_le s_float = { .single_float = x };
+  char *ptr_r = (char *)&reversed;
+  float_unmask_le s_float = {.single_float = x};
 
   printf("Floating point number: %f", x);
 
